858f0e5f85d5947ebe8a530c5283e5e0ad8ce489
hellenribas <tchwtrabalho@gmail.com>
2021-12-02 15:30:39 -0300

    requisito 2 ok

--- src/challenges.js
+++ src/challenges.js
@@ -1,17 +1,17 @@
 // Desafio 1
 function compareTrue(a, b) {
   if (a == true && b == true) {
-    return true
+    return true;
   } else {
-    return false
+    return false;
   }
 }
 
 // Desafio 2
-function calcArea() {
-  // seu código aqui
+function calcArea(base, height) {
+  let area = (base * height) / 2;
+  return area;
 }
-
 // Desafio 3
 function splitSentence() {
   // seu código aqui
